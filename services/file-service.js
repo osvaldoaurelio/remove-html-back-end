642975bc7d3326d5cc34234f23b7ba8c1b2e65f8
Osvaldo Aurélio Ribeiro Silva <31992337+osvaldoaurelio@users.noreply.github.com>
2024-06-14 13:43:35 -0300

    fix replace function

--- services/file-service.js
+++ services/file-service.js
@@ -5,15 +5,15 @@ const removeHtmlTags = file => {
   checkFileSize(file);
   checkFileType(file);
 
-  const content = file.buffer.toString('utf8');
-  // const content = file.buffer.toString('utf8').replace(/<br\b(?:.*?)>/gi, '\n$&');
-  const $ = Cheerio
-    .load(content)
+  const content = file.buffer.toString('utf8')
     .replace(/&nbsp;/gi, ' ')
     .replace(/&quot;/gi, '"')
     .replace(/&gt;/gi, '>')
     .replace(/&lt;/gi, '<')
     .replace(/&amp/gi, '&');
+
+  // const content = file.buffer.toString('utf8').replace(/<br\b(?:.*?)>/gi, '\n$&');
+  const $ = Cheerio.load(content);
   
   $('*').each(function () {
     $(this).replaceWith($(this).text());
