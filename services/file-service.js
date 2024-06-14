02c840d3c83bcc044a312ceacc02dfba13e8ba2f
Osvaldo Aurélio Ribeiro Silva <31992337+osvaldoaurelio@users.noreply.github.com>
2024-06-14 13:38:08 -0300

    add replacements

--- services/file-service.js
+++ services/file-service.js
@@ -7,13 +7,19 @@ const removeHtmlTags = file => {
 
   const content = file.buffer.toString('utf8');
   // const content = file.buffer.toString('utf8').replace(/<br\b(?:.*?)>/gi, '\n$&');
-  const $ = Cheerio.load(content);
+  const $ = Cheerio
+    .load(content)
+    .replace(/&nbsp;/gi, ' ')
+    .replace(/&quot;/gi, '"')
+    .replace(/&gt;/gi, '>')
+    .replace(/&lt;/gi, '<')
+    .replace(/&amp/gi, '&');
   
   $('*').each(function () {
     $(this).replaceWith($(this).text());
   });
 
-  return $.html().replace(/&nbsp;/gi, ' ').replace(/&amp/gi, '&');
+  return $.html();
 }
 
 module.exports = { removeHtmlTags };
